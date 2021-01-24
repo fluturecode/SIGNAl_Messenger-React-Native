f550f026de5164b08c262eef07cfd897e29cba3a
Elizabeth Eidelson <fluturecode@gmail.com>
2021-01-24 15:51:25 -0500

    feat(Login): added styling with elements

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,51 @@
-import React from "react"
+import React, { useState } from "react"
 import { StyleSheet, Text, View } from "react-native"
+import { Button, Input, Image } from "react-native-elements"
 
 const LoginScreen = () => {
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
+
 	return (
 		<View>
-			<Text>I am the login screen</Text>
+			<Image
+				style={{ width: 200, height: 200 }}
+				source={{
+					uri:
+						"https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
+				}}
+			/>
+			<View>
+				<Input
+					placeholder='Email'
+					autoFocus
+					type='email'
+					value={email}
+					onChangeText={(text) => setEmail(text)}
+				/>
+				<Input
+					placeholder='Password'
+					secureTextEntry
+					type='password'
+					value={password}
+					onChangeText={(text) => setPassword(text)}
+				/>
+				<Button containerStyle={styles.button} title='Login' />
+				<Button
+					containerStyle={styles.button}
+					title='Register'
+					type='outline'
+				/>
+			</View>
 		</View>
 	)
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	button: {
+		width: 200,
+		marginTop: 10,
+	},
+})
