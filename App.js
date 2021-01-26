eba8b98b9899bc3386c990f2252c26b1311ccfc2
Elizabeth Eidelson <fluturecode@gmail.com>
2021-01-26 10:39:23 -0500

    added global styling to screens

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
-import "react-native-gesture-handler"
-import { StatusBar } from "expo-status-bar"
-import React from "react"
-import { StyleSheet, Text, View } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
+import React from "react"
+import { StyleSheet } from "react-native"
+import "react-native-gesture-handler"
 import LoginScreen from "./screens/LoginScreen"
 
 const Stack = createStackNavigator()
 
+const globalScreenOptons = {
+	headerStyle: { backgroundColor: "#2c6bed" },
+	headerTitleStyle: { color: "white" },
+	headerTintColor: "white",
+}
+
 export default function App() {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator>
+			<Stack.Navigator screenOptions={globalScreenOptons}>
 				<Stack.Screen name='Login' component={LoginScreen} />
 			</Stack.Navigator>
 		</NavigationContainer>
