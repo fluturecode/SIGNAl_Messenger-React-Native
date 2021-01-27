1733b872a5af59f3f39bef6e14c23da6836c302c
Elizabeth Eidelson <fluturecode@gmail.com>
2021-01-27 13:21:42 -0500

    added expo vector icons to HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,3 +1,4 @@
+import { AntDesign, SimpleLineIcons } from "@expo/vector-icons"
 import React, { useLayoutEffect } from "react"
 import {
 	SafeAreaView,
@@ -11,6 +12,11 @@ import CustomListItem from "../components/CustomListItem"
 import { auth } from "../firebase"
 
 const HomeScreen = ({ navigation }) => {
+	const signOutUser = () => {
+		auth.signOut().then(() => {
+			navigation.replace("Login")
+		})
+	}
 	useLayoutEffect(() => {
 		navigation.setOptions({
 			title: "Signal",
@@ -19,13 +25,29 @@ const HomeScreen = ({ navigation }) => {
 			headerTintColor: "black",
 			headerLeft: () => (
 				<View style={{ marginLeft: 20 }}>
-					<TouchableOpacity>
+					<TouchableOpacity onPress={signOutUser} activeOpactiy={0.5}>
 						<Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
 					</TouchableOpacity>
 				</View>
 			),
+			headerRight: () => (
+				<View
+					style={{
+						flexDirection: "row",
+						justifyContent: "space-between",
+						width: 80,
+						marginRight: 20,
+					}}>
+					<TouchableOpacity activeOpactiy={0.5}>
+						<AntDesign name='camerao' size={24} color='black' />
+					</TouchableOpacity>
+					<TouchableOpacity activeOpactiy={0.5}>
+						<SimpleLineIcons name='pencil' size={24} color='black' />
+					</TouchableOpacity>
+				</View>
+			),
 		})
-	}, [])
+	}, [navigation])
 
 	return (
 		<SafeAreaView>
