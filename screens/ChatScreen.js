b97ae95470217cb7de0bd4baeacc8aed2a89ec6a
Elizabeth Eidelson <fluturecode@gmail.com>
2021-02-01 23:33:36 -0500

    added functionality to ChatScreen.js icons

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,9 +1,22 @@
 import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons"
-import React, { useLayoutEffect } from "react"
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import React, { useLayoutEffect, useState } from "react"
+import {
+	KeyboardAvoidingView,
+	Platform,
+	SafeAreaView,
+	ScrollView,
+	StatusBar,
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
+} from "react-native"
 import { Avatar } from "react-native-elements"
 
 const ChatScreen = ({ navigation, route }) => {
+	const [input, setInput] = useState("")
+
 	useLayoutEffect(() => {
 		navigation.setOptions({
 			title: "Chat",
@@ -22,7 +35,7 @@ const ChatScreen = ({ navigation, route }) => {
 								"https:/cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
 						}}
 					/>
-					<Text style={{ color: "white", marginLef: 10, fontWeight: "700" }}>
+					<Text style={{ color: "white", marginLeft: 10, fontWeight: "700" }}>
 						{route.params.chatName}
 					</Text>
 				</View>
@@ -53,13 +66,38 @@ const ChatScreen = ({ navigation, route }) => {
 		})
 	}, [navigation])
 
+	const sendMessage = () => {}
+
 	return (
-		<View>
-			<Text>{route.params.chatName}</Text>
-		</View>
+		<SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+			<StatusBar style='light' />
+			<KeyboardAvoidingView
+				behavior={Platform.OS === "ios" ? "padding" : "height"}
+				style={styles.container}
+				keyboardVerticalOffset={90}>
+				<>
+					<ScrollView>{/*Chat goes here*/}</ScrollView>
+					<View style={styles.footer}>
+						<TextInput
+							value={input}
+							onChangeText={(text) => setInput(text)}
+							placeholder='Signal Message'
+							style={styles.textInput}
+						/>
+						<TouchableOpacity onPress={sendMessage} activeOpactiy={0.5}>
+							<Ionicons name='send' size={24} color='#2B68E6' />
+						</TouchableOpacity>
+					</View>
+				</>
+			</KeyboardAvoidingView>
+		</SafeAreaView>
 	)
 }
 
 export default ChatScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	container: {},
+	footer: {},
+	textInput: {},
+})
