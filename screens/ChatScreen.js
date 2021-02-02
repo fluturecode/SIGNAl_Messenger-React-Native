16bb5ae3fa74c56dd3926649a8ae2f7dd23fe75b
Elizabeth Eidelson <fluturecode@gmail.com>
2021-02-01 22:53:45 -0500

    added headerTitle to ChatScreen.js

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,5 +1,6 @@
 import React, { useLayoutEffect } from "react"
 import { StyleSheet, Text, View } from "react-native"
+import { Avatar } from "react-native-elements"
 
 const ChatScreen = ({ navigation, route }) => {
 	useLayoutEffect(() => {
@@ -7,6 +8,24 @@ const ChatScreen = ({ navigation, route }) => {
 			title: "Chat",
 			headerBackTitleVisible: false,
 			headerTitleAlign: "left",
+			headerTitle: () => (
+				<View
+					style={{
+						flexDirection: "row",
+						alignItems: "center",
+					}}>
+					<Avatar
+						rounded
+						source={{
+							uri:
+								"https:/cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
+						}}
+					/>
+					<Text style={{ color: "white", marginLef: 10, fontWeight: "700" }}>
+						{route.params.chatName}
+					</Text>
+				</View>
+			),
 		})
 	}, [navigation])
 
